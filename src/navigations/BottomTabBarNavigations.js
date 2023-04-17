74c1ea9ede76ae9ee38bd9f8b7e1c081cf700db9
Berkay <berkay.ergun16@gmail.com>
2023-04-17 16:23:18 +0300

    tabbar icon svg added

--- src/navigations/BottomTabBarNavigations.js
+++ src/navigations/BottomTabBarNavigations.js
@@ -2,6 +2,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { BOTTOM_TAB_BAR_ROUTES } from './constant';
 import { BottomTabBarRoutes } from '../routes/BottomTabBarRoutes';
 import { StyleSheet } from 'react-native';
+import { getBottomTabBarScreenOptions } from './screenOptions/BottomTabBarScreenOptions';
 
 const Tab = createBottomTabNavigator();
 
@@ -9,19 +10,7 @@ const BottomTabBarNavigations = () => {
     return (
         <Tab.Navigator
             initialRouteName={BOTTOM_TAB_BAR_ROUTES.HomeStack}
-            screenOptions={{
-                tabBarShowLabel: false,
-                headerTitleAlign: 'left',
-                headerTitle: 'E-Market',
-                headerTitleStyle: {
-                    fontWeight: 'bold',
-                    color: '#FFFFFF',
-                    fontSize: 24,
-                },
-                headerStyle:{
-                    backgroundColor: '#2A59FE',
-                }
-            }}
+            screenOptions={({ route }) => getBottomTabBarScreenOptions(route)}
         >
 
             {
